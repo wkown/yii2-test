19e08e411fed7b0faddad7242ace56bd59d1c1c9
walkskyer <walkskyer@qq.com>
2016-10-11 10:02:12 +0800

    feat(admin/AuthTestController):增加ACF于RBAC配合使用的示例

--- public/modules/admin/config/web.php
+++ public/modules/admin/config/web.php
@@ -6,6 +6,7 @@
  * Time: 21:33
  */
 $config = [
+        'homeUrl'=>'/admin',
     'components' => [
         'user' => [
             'class' => 'yii\web\User',
--- public/modules/admin/controllers/AuthTestController.php
+++ public/modules/admin/controllers/AuthTestController.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * RBAC 权限测试.
+ *
+ * 规则设置 参见 app/commands/RbacController::actionAuthTest()
  * User: walkskyer
  * Date: 2016/10/10
  * Time: 16:28
@@ -9,15 +11,41 @@
 namespace app\modules\admin\controllers;
 use Yii;
 use yii\base\Module;
+use yii\filters\AccessControl;
 
 class AuthTestController extends Controller
 {
     public function __construct($id, Module $module, array $config=[])
     {
         parent::__construct($id, $module, $config);
-        if(!Yii::$app->getUser()->can(Yii::$app->requestedRoute)){
-            die('deny:'.Yii::$app->requestedRoute);
+        $requestedRoute = Yii::$app->requestedRoute;
+        $skipAction = strpos($requestedRoute,'admin/auth-test/c')===false;
+        if($skipAction && !Yii::$app->getUser()->can($requestedRoute)){
+            die('deny:'.$requestedRoute);
         }
+    }
+
+    //用于测试ACF于RBAC如何配合
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['c1', 'c2', 'c3','c4'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['c1', 'c2'],
+                        'roles' => ['a'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['c3','c4'],
+                        'roles' => ['b'],
+                    ],
+                ],
+            ],
+        ];
     }
 
     public function actionA1(){
@@ -34,4 +62,17 @@ class AuthTestController extends Controller
         return $this->render('test',['msg'=>'B2']);
     }
 
+    //用于测试ACF于RBAC如何配合
+    public function actionC1(){
+        return $this->render('test',['msg'=>'C1']);
+    }
+    public function actionC2(){
+        return $this->render('test',['msg'=>'C2']);
+    }
+    public function actionC3(){
+        return $this->render('test',['msg'=>'C3']);
+    }
+    public function actionC4(){
+        return $this->render('test',['msg'=>'C4']);
+    }
 }
